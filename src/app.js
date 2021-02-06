ba19b67614041a93c500aa6d8366ba399e382b4c
Leonardo Pessoa <leonardopessoa799@gmail.com>
2021-02-06 19:07:22 -0300

    tabnav OO

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import SmoothScroll from './modules/smooth-scroll.js';
 import Accordion from './modules/accordion.js';
-import initTabNav from './modules/tabnav.js';
+import TabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
@@ -16,7 +16,9 @@ smoothScroll.init();
 const accordion = new Accordion('[data-anime="according"] dt');
 accordion.init();
 
-initTabNav();
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
+
 initModal();
 initTooltip();
 initDropdownMenu();
